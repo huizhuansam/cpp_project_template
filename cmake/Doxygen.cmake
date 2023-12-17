e53bc5ec01c89c5d4a3079315ae84e668c67b72d
Hui Zhuan <huizhuansam@hotmail.com>
2023-12-17 20:31:23 +0800

    add Doxygen cmake

---
+++ cmake/Doxygen.cmake
@@ -0,0 +1,24 @@
+find_package(Doxygen)
+
+if (NOT DOXYGEN_FOUND)
+  message("Doxygen need to be installed to generate the doxygen documentation")
+else()
+  # to be referenced as variables in Doxyfile
+  set(PROJECT_SOURCE_DIR "src")
+  set(PROJECT_INCLUDE_DIR "include")
+
+  # set input and output files
+  set(DOXYGEN_IN ${CMAKE_CURRENT_SOURCE_DIR}/docs/Doxyfile)
+  set(DOXYGEN_OUT ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile.out)
+
+  # request to configure the file
+  configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
+  message("Doxygen build started")
+
+  # Note: do not put "ALL" - this builds docs together with application EVERY TIME!
+  add_custom_target(docs
+    COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    COMMENT "Generating API documentation with Doxygen"
+    VERBATIM)
+endif()
