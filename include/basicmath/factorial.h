2324086a90ea8f4e2e724ff6b68b5fee1dc0a0f7
Hui Zhuan <huizhuansam@hotmail.com>
2023-12-17 20:32:21 +0800

    add documentation comments

--- include/basicmath/factorial.h
+++ include/basicmath/factorial.h
@@ -1,5 +1,9 @@
 #pragma once
 
 namespace basicmath {
+
+/// @brief Computes the factorial of an integer.
+/// @param n
+/// @return n!
 int factorial(int n);
-}
+}  // namespace basicmath
--- include/basicmath/modulus.h
+++ include/basicmath/modulus.h
@@ -1,5 +1,9 @@
 #pragma once
 
 namespace basicmath {
+
+/// @brief Computes the modulus of an integer.
+/// @param x
+/// @return |x|
 int modulus(int x);
-}
+}  // namespace basicmath
