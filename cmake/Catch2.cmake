4998031a53c8a81a2fce592bcf6abc55a3c4f85b
Hui Zhuan <huizhuansam@hotmail.com>
2023-12-17 14:39:36 +0800

    exclude tests from release build

---
+++ cmake/Catch2.cmake
@@ -0,0 +1,6 @@
+# excludes tests from release build 
+if (NOT CMAKE_BUILD_TYPE STREQUAL "Release")
+  add_subdirectory(lib)
+  message(STATUS "Building tests...")
+  add_subdirectory(tests)
+endif()
