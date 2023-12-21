025069b4d8176a2fe00092849c437fc3dc9e53bb
Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2023-12-21 17:49:32 +0800

    add Simulation

---
+++ simulation/include/Simulation.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include "Event.hpp"
+
+class Simulation {
+ public:
+  virtual std::vector<std::shared_ptr<Event>> getInitialEvents() = 0;
+};
