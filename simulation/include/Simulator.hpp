512653cca0253baf87a47eba46cc00c319b3fc1c
Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2023-12-28 13:38:26 +0800

    utilize MyCompare for priority queue

--- simulation/include/Simulator.hpp
+++ simulation/include/Simulator.hpp
@@ -3,12 +3,18 @@
 #include <memory>
 #include <queue>
 #include "Event.hpp"
+#include "MyCompare.hpp"
 #include "Simulation.hpp"
 
 class Simulator {
  private:
-  std::priority_queue<std::shared_ptr<Event>> events;
-  explicit Simulator(std::priority_queue<std::shared_ptr<Event>> events);
+  std::priority_queue<std::shared_ptr<Event>,
+                      std::vector<std::shared_ptr<Event>>,
+                      MyCompare>
+      events;
+  explicit Simulator(std::priority_queue<std::shared_ptr<Event>,
+                                         std::vector<std::shared_ptr<Event>>,
+                                         MyCompare> events);
 
  public:
   static Simulator factory(const Simulation& simulation);
--- simulation/src/Simulator.cpp
+++ simulation/src/Simulator.cpp
@@ -1,25 +1,30 @@
 #include "Simulator.hpp"
 #include <iostream>
+#include <utility>
 
-Simulator::Simulator(std::priority_queue<std::shared_ptr<Event>> events)
+Simulator::Simulator(std::priority_queue<std::shared_ptr<Event>,
+                                         std::vector<std::shared_ptr<Event>>,
+                                         MyCompare> events)
     : events(events){};
 
 Simulator Simulator::factory(const Simulation& simulation) {
-  std::priority_queue<std::shared_ptr<Event>> events{};
+  std::priority_queue<std::shared_ptr<Event>,
+                      std::vector<std::shared_ptr<Event>>, MyCompare>
+      events{};
   for (const std::shared_ptr<Event> event : simulation.getInitialEvents()) {
     events.push(event);
   }
-  return Simulator{(events)};
+  Simulator simulator{events};
+  return simulator;
 }
 
 void Simulator::run() {
   while (!(this->events.empty())) {
     std::shared_ptr<Event> event = this->events.top();
     this->events.pop();
-    std::cout << *event;
-    std::vector<std::shared_ptr<Event>> newEvents{event->simulate()};
-    for (const std::shared_ptr<Event> e : newEvents) {
+    for (const std::shared_ptr<Event> e : event->simulate()) {
       this->events.push(e);
     }
+    std::cout << *event;
   }
 }
