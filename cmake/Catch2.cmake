d38c84894fa42bfa32a7316c7b6633499b13a58a
Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2023-12-28 13:59:11 +0800

    move tests subdirectory

--- cmake/Catch2.cmake
+++ cmake/Catch2.cmake
@@ -2,5 +2,5 @@
 if (NOT CMAKE_BUILD_TYPE STREQUAL "Release")
   add_subdirectory(lib)
   message(STATUS "Building tests...")
-  add_subdirectory(tests)
+  add_subdirectory(simulation/tests)
 endif()
