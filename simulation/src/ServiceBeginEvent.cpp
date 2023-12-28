d7a8ce9963bb76ec6d86b966a1ea929adf86c326
Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2023-12-28 13:56:41 +0800

    integrate ServiceDoneEvent

--- simulation/src/ServiceBeginEvent.cpp
+++ simulation/src/ServiceBeginEvent.cpp
@@ -1,5 +1,6 @@
 #include "ServiceBeginEvent.hpp"
 #include "DepartureEvent.hpp"
+#include "ServiceDoneEvent.hpp"
 
 ServiceBeginEvent::ServiceBeginEvent(const double time,
                                      std::shared_ptr<Customer> customer,
@@ -8,8 +9,10 @@ ServiceBeginEvent::ServiceBeginEvent(const double time,
 
 std::vector<std::shared_ptr<Event>> ServiceBeginEvent::simulate() {
   this->counter->makeUnavailable();
-  return std::vector<std::shared_ptr<Event>>{std::make_shared<DepartureEvent>(
-      this->getTime() + this->customer->getServiceTime(), this->customer)};
+
+  return std::vector<std::shared_ptr<Event>>{std::make_shared<ServiceDoneEvent>(
+      this->getTime() + this->customer->getServiceTime(), this->customer,
+      this->counter)};
 }
 
 std::ostream& ServiceBeginEvent::print(std::ostream& out) const {
