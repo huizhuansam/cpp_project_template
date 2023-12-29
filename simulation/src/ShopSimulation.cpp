2d4f4fadc207172470a0d3bf4254aacea3d2ab96
Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2023-12-29 19:47:08 +0800

    integrate Parser

--- simulation/src/ShopSimulation.cpp
+++ simulation/src/ShopSimulation.cpp
@@ -4,25 +4,22 @@
 
 #include "ArrivalEvent.hpp"
 #include "Customer.hpp"
+#include "Parser.hpp"
 
 ShopSimulation::ShopSimulation(const std::shared_ptr<Shop> shop,
                                std::vector<std::shared_ptr<Event>> initEvents)
     : shop(shop), initEvents(initEvents) {}
 
 ShopSimulation ShopSimulation::factory() {
-  int numInitialEvents{};
-  std::cin >> numInitialEvents;
+  int numInitialEvents = Parser<int>::parse();
   std::vector<std::shared_ptr<Event>> initEvents(
       static_cast<unsigned long>(numInitialEvents));
-  int numCounters{};
-  std::cin >> numCounters;
+  int numCounters = Parser<int>::parse();
   std::shared_ptr<Shop> shop = std::make_shared<Shop>(numCounters);
   for (unsigned long i = 0; i < static_cast<unsigned long>(numInitialEvents);
        ++i) {
-    double arrivalTime{};
-    std::cin >> arrivalTime;
-    double serviceTime{};
-    std::cin >> serviceTime;
+    double arrivalTime = Parser<double>::parse();
+    double serviceTime = Parser<double>::parse();
     std::shared_ptr<Customer> customer =
         std::make_shared<Customer>(serviceTime);
     std::shared_ptr<Event> event =
