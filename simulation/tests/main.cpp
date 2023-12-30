99b9c2d4b3076725bbd5948a8d83054f5451f97a
Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2023-12-30 14:01:38 +0800

    add unit tests for CheckoutCounter

--- simulation/tests/main.cpp
+++ simulation/tests/main.cpp
@@ -1,2 +1,22 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+
+// clang-format off
+#include "CheckoutCounter.hpp"
+
+// clang-format on
+TEST_CASE("CheckoutCounter unit tests") {
+  CheckoutCounter cc{};
+  SECTION("CheckoutCounter is available when constructed") {
+    REQUIRE((cc.isAvailable() == true));
+  }
+  SECTION("CheckoutCounter can be made unavailable") {
+    cc.makeUnavailable();
+    REQUIRE((cc.isAvailable() == false));
+  }
+  SECTION("CheckoutCounter can be made available") {
+    cc.makeUnavailable();
+    cc.makeAvailable();
+    REQUIRE((cc.isAvailable() == true));
+  }
+}
