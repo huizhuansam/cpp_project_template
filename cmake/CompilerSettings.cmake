97c6a3be8c75a7ed7a7163df5a9bdb1b33bd9481
Hui Zhuan <64345999+huizhuansam@users.noreply.github.com>
2024-01-23 09:37:39 +0800

    add code coverage flags

--- cmake/CompilerSettings.cmake
+++ cmake/CompilerSettings.cmake
@@ -1,5 +1,8 @@
 if (NOT MSVC)
-  set(CMAKE_CXX_FLAGS_DEBUG -g)
+  # debug mode compiler flags
+  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -g")
+  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fprofile-instr-generate -fcoverage-mapping")
+
   set(CMAKE_CXX_FLAGS_RELEASE "\
     -O3 \
     -Wall \
